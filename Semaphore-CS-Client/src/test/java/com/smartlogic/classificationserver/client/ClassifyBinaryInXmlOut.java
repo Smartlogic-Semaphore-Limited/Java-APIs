cf8bc360e7e99a0c822cafe1541dda7130ba1a0d
Steve Carton <steve.carton@smartlogic.com>
2018-12-19 09:54:29 -0500

    test cases

--- Semaphore-CS-Client/src/test/java/com/smartlogic/classificationserver/client/ClassifyBinaryInXmlOut.java
+++ Semaphore-CS-Client/src/test/java/com/smartlogic/classificationserver/client/ClassifyBinaryInXmlOut.java
@@ -4,6 +4,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,5 +35,64 @@ public class ClassifyBinaryInXmlOut extends ClassificationTestCase {
 		byte[] xmlb = classificationClient.getClassificationServerResponse(byteArrayOutputStream.toByteArray(), "", null, null);
 		assert xmlb != null;
 		XMLReader.getDocument(xmlb);
+	}
+	
+	@Test
+	public void testBodyInBytesOut() throws IOException, ClassificationException {
+		File file = new File("src/test/resources/data/sample.pdf");
+		FileInputStream fileInputStream = new FileInputStream(file);
+		ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+		int available;
+		while ((available = fileInputStream.available()) > 0) {
+			byte[] data = new byte[available];
+			fileInputStream.read(data);
+			byteArrayOutputStream.write(data);
+		}
+		fileInputStream.close();
+		byteArrayOutputStream.close();
+		byte[] xmlb = classificationClient.getClassificationServerResponse(byteArrayOutputStream.toByteArray(), "", null, null);
+		assert xmlb != null;
+		XMLReader.getDocument(xmlb);
+	}
+	
+	private Map<String, Collection<String>> makeMetaData(Properties props) {
+    	Map<String, Collection<String>> metadata =  new HashMap<>();
+    	String[] metaflds = {"meta1","meta2"};
+    	for (String mdf : metaflds) {
+    		String val = props.getProperty(mdf);
+    		if (val!=null && val.length()>0) {
+    			Collection<String> vals = new ArrayList<>(); 
+    			for (String v : val.split(","))
+    				vals.add(v);
+    			metadata.put(mdf, vals);
+    		}
+    	}
+    	return metadata;
+
+	}
+	
+	@Test
+	public void testBodyTitleMetaInBytesOut() throws IOException, ClassificationException {
+		Properties props = new Properties();
+		File file = new File("src/test/resources/data/testBodyTestInBytesOut.txt");
+		FileInputStream fileInputStream = new FileInputStream(file);
+		props.load(fileInputStream);
+		fileInputStream.close();
+		byte[] xmlb = classificationClient.getClassifiedBytes(new Body(props.getProperty("body")), new Title(props.getProperty("title")), makeMetaData(props));
+		assert xmlb != null;
+		XMLReader.getDocument(xmlb);
+		
+	}
+	@Test
+	public void testUrlTitleMetaInBytesOut() throws IOException, ClassificationException {
+		Properties props = new Properties();
+		File file = new File("src/test/resources/data/testBodyTestInBytesOut.txt");
+		FileInputStream fileInputStream = new FileInputStream(file);
+		props.load(fileInputStream);
+		fileInputStream.close();
+		byte[] xmlb = classificationClient.getClassifiedBytes(new URL(props.getProperty("url")), new Title(props.getProperty("title")), makeMetaData(props));
+		assert xmlb != null;
+		XMLReader.getDocument(xmlb);
+		
 	}
 }
