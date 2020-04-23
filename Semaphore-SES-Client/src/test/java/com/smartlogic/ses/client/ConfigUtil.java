0aacb4f19529b51f23abffe5fa4a5edb6e21fb1c
Keith Atkins <keith.atkins@smartlogic.com>
2020-04-23 15:41:06 +0100

    Addition of Java 11 required dependencies for SES client

--- Semaphore-SES-Client/src/test/java/com/smartlogic/ses/client/ConfigUtil.java
+++ Semaphore-SES-Client/src/test/java/com/smartlogic/ses/client/ConfigUtil.java
@@ -1,43 +1,25 @@
 package com.smartlogic.ses.client;
 
-import java.io.InputStream;
-import java.util.Properties;
-
 public class ConfigUtil {
 
-    private static SESClient sesClient = getSESClient();
+	private static SESClient sesClient = getSESClient();
 
-    public static SESClient getSESClient() {
-        if (sesClient != null)
-            return sesClient;
+	public static SESClient getSESClient() {
+		if (sesClient != null)
+			return sesClient;
 
-        SESClient client = new SESClient();
-        Properties prop = new Properties();
+		SESClient client = new SESClient();
 
-        try (InputStream in = ConfigUtil.class.getClass().getResourceAsStream("/testconfig.properties")) {
-            prop.load(in);
+		client.setOntology("IPSV");
+		client.setHost("svrka02");
+		client.setPort(8983);
+		client.setPath("/ses");
 
-            client.setOntology(prop.getProperty("ontology"));
-            client.setHost(prop.getProperty("host"));
-            client.setPort(prop.getProperty("port"));
-            client.setPath(prop.getProperty("path"));
 
-            if (prop.containsKey("protocol"))
-                client.setProtocol(prop.getProperty("protocol"));
-            if (prop.containsKey("language"))
-               client.setLanguage(prop.getProperty("language"));
-            if (prop.containsKey("connectionTimeoutMs"))
-                client.setConnectionTimeoutMS(Integer.parseInt(prop.getProperty("connectionTimeoutMs")));
-            if (prop.containsKey("socketTimeoutMs"))
-                client.setConnectionTimeoutMS(Integer.parseInt(prop.getProperty("socketTimeoutMs")));
-
-            if (prop.containsKey("proxyHost"))
-                client.setProxyHost(prop.getProperty("proxyHost"));
-            if (prop.containsKey("proxyPort"))
-                client.setProxyHost(prop.getProperty("proxyPort"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return client;
-    }
+//		if (prop.containsKey("proxyHost"))
+//			client.setProxyHost(prop.getProperty("proxyHost"));
+//		if (prop.containsKey("proxyPort"))
+//			client.setProxyHost(prop.getProperty("proxyPort"));
+		return client;
+	}
 }
