f2b9df264dec0e8924e0fe52804a00d828f3af75
Keith Atkins <keith.atkins@smartlogic.com>
2020-04-29 13:55:20 +0100

    All OE tests now run against the three configured OE/KMM instances.
    The tests must be run manually.
    
    No bespoke classes, metadata types, relationship types are considered. These features need to be added.

--- Semaphore-OE-Client/src/test/java/com/smartlogic/ontologyeditor/examples/GetChangesSince.java
+++ Semaphore-OE-Client/src/test/java/com/smartlogic/ontologyeditor/examples/GetChangesSince.java
@@ -1,27 +1,36 @@
 package com.smartlogic.ontologyeditor.examples;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
 
+import com.smartlogic.cloud.CloudException;
 import com.smartlogic.ontologyeditor.OEClientException;
 import com.smartlogic.ontologyeditor.OEClientReadWrite;
 import com.smartlogic.ontologyeditor.beans.ChangeRecord;
 
 public class GetChangesSince extends ModelManipulation {
-	public static void main(String args[]) throws URISyntaxException, OEClientException, ParseException, IOException {
-		OEClientReadWrite oeClient = getOEClient(false);
+	public static void main(String args[]) throws IOException, CloudException, OEClientException {
+		runTests(new GetChangesSince());
+	}
+
+	@Override
+	protected void alterModel(OEClientReadWrite oeClient) throws OEClientException {
 
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		Date date = simpleDateFormat.parse("2017-06-01");
-		
-		Collection<ChangeRecord> changeRecords = oeClient.getChangesSince(date);
+		Date date;
+		try {
+			date = simpleDateFormat.parse("2017-06-01");
 
-		for (ChangeRecord changeRecord: changeRecords) {
-			System.out.println(changeRecord);
+			Collection<ChangeRecord> changeRecords = oeClient.getChangesSince(date);
+
+			for (ChangeRecord changeRecord : changeRecords) {
+				System.err.println(changeRecord);
+			}
+		} catch (ParseException e) {
+			System.err.println("Date parse exception - shouldn't happen");
 		}
 	}
 
